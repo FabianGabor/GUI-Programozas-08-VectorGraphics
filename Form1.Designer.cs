645f402c31d61e77c9169c68045d8dd72e42300b
Fábián Gábor <info@fabiangabor.com>
2021-11-02 20:14:00 +0100

    rect + ellipse

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,40 +1,50 @@
 ﻿namespace VectorGraphics
 {
-  partial class Form1
-  {
-    /// <summary>
-    /// Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-      if (disposing && (components != null))
-      {
-        components.Dispose();
-      }
-      base.Dispose(disposing);
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
-    }
-
-    #endregion
-  }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,92 @@ using System.Windows.Forms;
 
 namespace VectorGraphics
 {
-  public partial class Form1 : Form
-  {
-    public Form1()
+    struct graphicElement
     {
-      InitializeComponent();
+        private Color color;
+        private Point point1, point2;
+
+        public graphicElement(Point point1, Point point2) : this()
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+        }
+
+        public graphicElement(Color color, Point point1, Point point2)
+        {
+            this.color = color;
+            this.point1 = point1;
+            this.point2 = point2;
+        }
+
+        public Color Color => color;
+
+        public Point Point1 => point1;
+
+        public Point Point2 => point2;
     }
-  }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw(object sender, PaintEventArgs e)
+        {
+            graphicElement window = new graphicElement(new Point(0, 0), new Point(ClientSize.Width, ClientSize.Height));
+
+            Graphics graphics = e.Graphics;
+            drawCross(graphics, window);
+            drawRect(graphics, window);
+            drawCircle(graphics, window);
+        }
+
+        private void drawCross(Graphics graphics, graphicElement window)
+        {
+            Color color = Color.Red;
+            Pen pen = new Pen(color);
+
+            graphicElement lineHorizontal = new graphicElement(color, new Point(window.Point1.X, window.Point2.Y / 2),
+                new Point(window.Point2.X, window.Point2.Y / 2));
+            graphicElement lineVertical = new graphicElement(color, new Point(window.Point2.X / 2, window.Point1.Y),
+                new Point(window.Point2.X / 2, window.Point2.Y));
+
+            graphics.DrawLine(pen, lineHorizontal.Point1, lineHorizontal.Point2);
+            graphics.DrawLine(pen, lineVertical.Point1, lineVertical.Point2);
+        }
+
+        private void drawRect(Graphics graphics, graphicElement window)
+        {
+            Color color = Color.Blue;
+            Pen pen = new Pen(color);
+
+            Padding padding = new Padding(3);
+            Rectangle rectangle = new Rectangle(window.Point1.X, window.Point1.Y, window.Point2.X, window.Point2.Y);
+            rectangle.Inflate(-padding.Size.Width, -padding.Size.Height);
+            graphics.DrawRectangle(pen, rectangle);
+        }
+
+        private void drawCircle(Graphics graphics, graphicElement window)
+        {
+            Color color = Color.Green;
+            Pen pen = new Pen(color);
+
+            Padding padding = new Padding(3);
+            Rectangle rectangle = new Rectangle(window.Point1.X, window.Point1.Y, window.Point2.X, window.Point2.Y);
+            rectangle.Inflate(-padding.Size.Width, -padding.Size.Height);
+            graphics.DrawEllipse(pen, rectangle);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(sender, e);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+    }
 }
